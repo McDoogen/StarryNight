0adcbf8987af25827d791f1562f33e357fa56aa2
Douglas Smith <McDoogen@gmail.com>
2020-08-22 12:08:39 -0400

    Night time control

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ print("Running")
 
 while True:
 	now = time.localtime()
-	if(now.tm_min >= 56):
-		signal.on()
-	else:
+	if(now.tim_hour >= 19 or now.tim_hour < 1):
 		signal.off()
+	else:
+		signal.on()
 		
