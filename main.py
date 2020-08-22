29fdd9dbf57daf870b82de24edc2a805822e1f0d
Douglas Smith <McDoogen@gmail.com>
2020-08-22 12:45:41 -0400

    argv

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from gpiozero import DigitalOutputDevice
-import time
+import time, sys
 
 signal = DigitalOutputDevice(14)
 
 print("Running")
 
+if(sys.argv[1] == 0):
+	signal.on()
+elif(sys.argv[1] == 1):
+	signal.off()
+
 while True:
-	now = time.localtime()
-	if(now.tim_hour >= 19 or now.tim_hour < 1):
-		signal.off()
-	else:
-		signal.on()
+	#now = time.localtime()
+	#if(now.tim_hour >= 19 or now.tim_hour < 1):
+	#	signal.off()
+	#else:
+	#	signal.on()
 		
