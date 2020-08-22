8acf114a6caa6a14c7712be39413c2a001f34399
Douglas Smith <McDoogen@gmail.com>
2020-08-22 18:00:40 -0400

    adjust

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import time, sys
 signal = DigitalOutputDevice(14)
 
 print("Running")
-print(sys.argv[1])
-if(sys.argv[1] == '0'):
-	print("Turn on")
-	signal.on()
-elif(sys.argv[1] == '1'):
-	print("Turn off")
-	signal.off()
+#print(sys.argv[1])
+#if(sys.argv[1] == '0'):
+#	print("Turn off")
+#	signal.on()
+#elif(sys.argv[1] == '1'):
+#	print("Turn on")
+#	signal.off()
 
 while True:
 	pass
-	#now = time.localtime()
-	#if(now.tim_hour >= 19 or now.tim_hour < 1):
-	#	signal.off()
-	#else:
-	#	signal.on()
+	now = time.localtime()
+	if(now.tim_hour >= 19 or now.tim_hour < 1):
+		signal.off()
+	else:
+		signal.on()
 		
